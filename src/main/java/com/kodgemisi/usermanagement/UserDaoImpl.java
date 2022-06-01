a64492718c6b99ab05eecad68891a5174382e380
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-06-01 10:59:20 +0300

    Fixed the update issue with a simple solution.

--- src/main/java/com/kodgemisi/usermanagement/UserDaoImpl.java
+++ src/main/java/com/kodgemisi/usermanagement/UserDaoImpl.java
@@ -56,7 +56,6 @@ public class UserDaoImpl implements UserDao {
 		if (user.isNew()) {
 			throw new IllegalArgumentException("You can only update existing users (users with id). This user has no id");
 		}
-
 		userMap.replace(user.getId(), user);
 		return user;
 	}
